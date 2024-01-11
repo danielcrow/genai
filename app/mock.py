57cb8e796e64320e6f2ae67a66567fbd86d7b7c7
daniel.j.crow@gmail.com <daniel.j.crow@gmail.com>
2024-01-11 11:46:15 +0000

    updates

--- app/mock.py
+++ app/mock.py
@@ -1,2 +1,11 @@
+
+from dotenv import load_dotenv, find_dotenv
+import os
+import json
+
+load_dotenv(find_dotenv())
+
 def getOpps():
+    #return os.getenv("OPPS")
+    #print( json.loads(os.getenv("OPPS")))
     return [{"OppId":"01010101",  "CustomerName":"Daniel Crow", "OpportunityName": "RB-Phantom"},{"OppId":"01010102",  "CustomerName":"Douglas Coombs", "OpportunityName": "Spectre"}]
