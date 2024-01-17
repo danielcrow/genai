0681568202bb7da1e0ec562e7e5acad0f4be0970
daniel.j.crow@gmail.com <daniel.j.crow@gmail.com>
2024-01-17 12:09:52 +0000

    stuff

--- app/carsales/carsales.py
+++ app/carsales/carsales.py
@@ -32,14 +32,14 @@ def getOpps():
     return {"opps" : [{"OppId":"01010101",  "CustomerName":"Daniel Crow", "OpportunityName": "Ghost"},{"OppId":"01010102",  "CustomerName":"Douglas Coombs", "OpportunityName": "Spectre"}]}
 
 
-@router.get("/getContactHistory",response_model=ContactHistory, summary="Get Contact Details", description="Get Contact Details", operation_id="GetContactDetails",openapi_extra=extendedTags)
+@router.get("/getContactHistory",response_model=ContactHistory, summary="Get Contact History", description="Get Contact History", operation_id="GetContactHistory",openapi_extra=extendedTags)
 def getContactDetails(Contact:str):
     if(Contact == "ABC123"):
         return {"Contact":"ABC123",  "CustomerName":"Daniel Crow","PreviousBuyer":True, "PreviousCar":"Ghost"}
     else:
         return {"Contact":"ABD123",  "CustomerName":"Douglas Coombs","PreviousBuyer":False, "PreviousCar":""}
 
-@router.get("/getCarSpec",response_model=CarSpec, summary="Get Contact Details", description="Get Contact Details", operation_id="GetContactDetails",openapi_extra=extendedTags)
+@router.get("/getCarSpec",response_model=CarSpec, summary="Get Car Specification", description="Get Car Specification", operation_id="GetCarSpecification",openapi_extra=extendedTags)
 def getContactDetails(OppId:str):
     if(OppId == "01010101"):
         return {"OppId":"01010101",  "CarModel":"Ghost",  "CarValue": 510000}
